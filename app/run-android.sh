e40a4a788c0cca1595cc08171078a5a5b39912fa
Martin Steen <martin.steen@ksti.de>
2021-11-22 17:13:43 +0100

    Initial commit

---
+++ app/run-android.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export ANDROID_SDK_ROOT=/home/steen/Android-Projekte/Android/Sdk
+export ANDROID_HOME=/home/steen/Android-Projekte/Android
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+cordova build android
+
